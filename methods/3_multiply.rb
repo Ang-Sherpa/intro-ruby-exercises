f23810b8056c465e36c722ea2db68b582bb84483
Ang Sherpa <anglamusherpa@gmail.com>
2023-06-16 11:54:30 -0600

    Finished exercises for methods

---
+++ methods/3_multiply.rb
@@ -0,0 +1,9 @@
+#Ex 3.  takes two arguments and returns the product of the two numbers.
+
+def multiply(a,b)
+  a * b 
+end 
+
+puts multiply(10,47)
+
+
---
+++ methods/5_scream_fix.rb
@@ -0,0 +1,10 @@
+#Ex 4
+
+def scream(words)
+  words = words + "!!!!"
+  puts words
+end
+
+scream("Yippeee")
+
+# return nil because the last evaluated expression is puts words. puts always returns nil. 
