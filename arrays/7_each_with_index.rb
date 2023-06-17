cb176ed7fc5393f1060bc9b23a194c98de226701
Ang Sherpa <anglamusherpa@gmail.com>
2023-06-16 18:45:54 -0600

    Finished exercies Arrays

---
+++ arrays/7_each_with_index.rb
@@ -0,0 +1,6 @@
+arr = [3, 6, 9, 12, 15, 18, 21, 24]
+
+arr.each_with_index do |val, i|
+  puts "#{i}. #{val}"
+end
+
---
+++ arrays/8_increment.rb
@@ -0,0 +1,7 @@
+arr = [5, 10, 15, 20, 25, 30]
+arr_2 = arr.map do |x|
+  x + 2
+end 
+
+p arr
+p arr_2
